1acd537a4a4cec0ce055b3ad00ac9a2ca3e16f2c
Flavio Pinus <Flavio Pinus>
2016-03-30 21:31:52 +0300

    functions for programmingassignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The main idea is that the inverse of a Matrix is solve()
+## After following the example of the mean and changing the specific things to adapt to a inverse matrix - that's what I got.
+## I must say that it would be almos impossible to get to this function by myself with only the 
+## Knowladge from the classes. I would like to know more to the point I could figure this out by myself
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- mean
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get
+  inv <- solve(data)
+  X$setinverse(inv)
+  inv
 }
